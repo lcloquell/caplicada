b4262242ce9da6a8423b7d04cedfa5f177240533
lcloquell <luciano.cloquell@mercadolibre.com>
2019-11-05 11:14:13 -0300

    palindromo

--- script-tp-integrador.sh
+++ script-tp-integrador.sh
@@ -24,17 +24,47 @@ function fibo(){
 		y=$t
 	done
 }
+
+function invertido(){
+	echo "Ingrese el numero"
+	read num
+	echo "$num"|rev
+}
+
+function palin() {
+	largo=0
+	i=0
+	read -p "Ingrese la cadena: " cadena
+	largo=`echo $cadena | wc -m`
+	largo=`expr $largo - 1`
+	if [ ! $largo -eq 0 ]; then
+		med=`expr $largo / 2`
+	    	while [ $i -le $med ]; do
+        		ca=`echo $cadena|cut -c$i`
+        		de=`echo $cadena|cut -c$largo`
+	        	if [ $ca != $de ]; then
+        	    		echo "No es palindromo"
+        		fi
+        		i=`expr $i + 1`
+	        	largo=`expr $largo - 1`
+	    	done
+	    	echo "Es palindromo"
+	fi
+}
 function saluda() {
 	who=$1
 	echo "Bye $who"
 	sleep 4
 }
 echo "Comienzo del script"
+clear
 menu
 while true; do
 	read -p "Menu: " numero
 	case $numero in
 		1) fibo;;
+		2) invertido;;
+		3) palin;;
 		7) saluda `whoami`
 			break;; 
 	esac
