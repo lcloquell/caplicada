4130fc4ebe3cd3e0930b1b6f396cc5b6433386d0
lcloquell <luciano.cloquell@mercadolibre.com>
2019-11-05 10:13:33 -0300

    menu, saluda

--- script-tp-integrador.sh
+++ script-tp-integrador.sh
@@ -1,5 +1,28 @@
 #/bin/bash
 
-echo "Comienzo del script"
+function menu(){
+	echo "1- Fibo"
+	echo "2- Invierte"
+	echo "3- Palindromo"
+	echo "4- Cantidad lineas"
+	echo "5- Ordenar 5 numeros"
+	echo "6- Contar archivos"
+	echo "7- Salir"
+}
 
+function saluda() {
+	who=$1
+	echo "Bye $who"
+	sleep 4
+}
+echo "Comienzo del script"
+menu
+while true; do
+	read -p "Menu: " numero
+	case $numero in
+		1) fibo;;
+		7) saluda `whoami`
+			break;; 
+	esac
+done
 echo "Fin del script"
